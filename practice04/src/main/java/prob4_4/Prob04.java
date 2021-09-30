c1413b035c98a70176201e6dc489231b04319546
kjw222 <whffu78@naver.com>
2021-09-30 13:58:26 +0900

    "6th"

--- practice04/src/main/java/prob4_4/Prob04.java
+++ practice04/src/main/java/prob4_4/Prob04.java
@@ -1,9 +1,9 @@
 package prob4_4;
 
 public class Prob04 {
-
-	public static void main(String[] args) {
-	//	Employee pt = new Depart( "홍길동", 3000, "개발부" );
-	//	pt.getInformation();
-	}
+//
+//	public static void main(String[] args) {
+//	//	Employee pt = new Depart( "홍길동", 3000, "개발부" );
+//	//	pt.getInformation();
+//	}
 }
